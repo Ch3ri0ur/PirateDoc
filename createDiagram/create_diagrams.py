141390de7d39efba198362b15bc8b4bdbc609ee1
Christian Wappler <22932267+Ch3ri0ur@users.noreply.github.com>
2020-10-14 18:32:50 +0200

    moved todo updated architecture diagram

--- createDiagram/create_diagrams.py
+++ createDiagram/create_diagrams.py
@@ -1,5 +1,8 @@
 import os
-os.environ["PATH"] += os.pathsep + 'C:\Program Files\Graphviz 2.44.1/bin/'
+
+os.environ["PATH"] += os.pathsep + "C:\Program Files\Graphviz 2.44.1/bin/"
+os.environ["PATH"] += os.pathsep + "C:/Program Files (x86)/Graphviz2.38/bin/"
+
 from diagrams import Cluster, Diagram, Edge
 from diagrams.custom import Custom
 from diagrams.onprem.analytics import Spark
@@ -23,16 +26,17 @@ graph_attr = {"fontsize": "45", "bgcolor": "transparent", "pad": "0.0"}
 with Diagram(filename="pirate_architecture", show=False, graph_attr=graph_attr):
     with Cluster("Pirate Module"):
         with Cluster("Raspberry PI"):
-            with Cluster("Docker1"):
+            # with Cluster("Docker1"):
+            with Cluster("Nodejs"):
                 nodeJs = NodeJS("Pirate-Bridge")
 
-            with Cluster("Docker2"):
+            with Cluster("Docker1"):
                 Janus = Custom("Pirate-Spyglass", "./customImages/janus-logo.png")
 
-            with Cluster("Docker3"):
+            with Cluster("Docker2"):
                 Gstreamer = Custom("Gstreamer", "./customImages/gstreamer-logo.png")
-                
-            Janus << Edge(label="RTSP Stream",color="firebrick") << Gstreamer
+
+            Janus << Edge(label="RTSP Stream", color="firebrick") << Gstreamer
             with Cluster("ReactJs"):
                 react = React("Pirate-Flag")
             caddy = Caddy("Reverse Proxy")
@@ -47,27 +51,24 @@ with Diagram(filename="pirate_architecture", show=False, graph_attr=graph_attr):
                 pid = Custom("PID Controller", "./customImages/720px-Arduino_Logo.png")
 
     # Data from Arduino to Pi
-    nodeJs << Edge(label="Pirate Serial",color="firebrick") >> arduino
+    nodeJs << Edge(label="Pirate Serial", color="firebrick") >> arduino
 
     Gstreamer << Edge(color="firebrick") << piCamera
 
     client = Client("User")
     client << Edge(color="firebrick") >> caddy
-    
+
 with Diagram(filename="pirate_overview", show=False, graph_attr=graph_attr):
     with Cluster("Pirate Module"):
         pi = Custom("Raspberry Pi", "./customImages/RPi-Logo-SCREEN.png")
-            
-            
-        
 
         with Cluster("Arduino Project"):
             piCamera = IotCamera("Camera")
             arduino = Custom("Pirate Hook", "./customImages/720px-Arduino_Logo.png")
 
-    pi << Edge(label="",color="firebrick") << piCamera
-    pi << Edge(label="Serial",color="firebrick") >> arduino
+    pi << Edge(label="", color="firebrick") << piCamera
+    pi << Edge(label="Serial", color="firebrick") >> arduino
 
     client = Client("User")
-    client << Edge(label="Webpage\n + \nWebRTC Stream",color="firebrick") >> pi
-    
+    client << Edge(label="Webpage\n + \nWebRTC Stream", color="firebrick") >> pi
+
